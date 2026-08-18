2829247487412e1bc338f182be1ce143b937358c
Forge <forge@webdavinci.com>
2026-08-18 10:03:21 +0200

    Add Matomo analytics tracking

--- resources/views/frontend/pages/layouts/app.blade.php
+++ resources/views/frontend/pages/layouts/app.blade.php
@@ -49,6 +49,22 @@
 <script src="{{ asset('assets/js/functions.js') }}"></script>
 <script type="text/javascript" src="{{ asset('assets/plugins/gmap3/gmap3.min.js') }}"></script>
 <script type="text/javascript" src="{{ asset('assets/plugins/gmap3/map-styles.js') }}"></script>
+
+<!-- Matomo -->
+<script>
+  var _paq = window._paq = window._paq || [];
+  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="//hits.webdavinci.com/";
+    _paq.push(['setTrackerUrl', u+'matomo.php']);
+    _paq.push(['setSiteId', '5']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+  })();
+</script>
+<!-- End Matomo Code -->
 </body>
 
 </html>
